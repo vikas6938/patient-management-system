9f7790c1fcfdf95d28806b082d4b18e9ef927ae9
vikas6938 <vikasborse000@gmail.com>
2024-11-06 22:42:07 +0530

    server changes

--- backend/src/server.js
+++ backend/src/server.js
@@ -30,7 +30,7 @@ const PORT = Config.PORT || 5000;
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cors({
-  origin: "https://patient-management-system-rho.vercel.app/", 
+  origin: "https://patient-management-system-rho.vercel.app", 
   methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true
 }));
